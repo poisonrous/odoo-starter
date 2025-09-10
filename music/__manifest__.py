64ba711760ea9bec1e8ee596fcb8c4589827b06c
Rosalinda <abreuparrarosalinda@gmail.com>
2025-09-10 15:39:43 -0400

    first commit

---
+++ music/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Music',
+    'version': '1.0',
+    'summary': 'idk, man, I like music',
+    'category': 'Music',
+    'installable': True,
+    'application': True,
+}
---
+++ music/models/music_album.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+class MusicAlbum(models.Model):
+    _name = 'music_album'
+    _description = 'Music Album'
+
+    name = fields.Char(
+        string='Name'
+        required=True
+    )
+    artist = fields.Char(
+        string='Artist'
+        required=True
+    )
+    release_date = fields.Date(
+        string='Release Date'
+        required=True
+    )
+    genre = fields.Selection(
+        string='Genre',
+        selection=[('pop','Pop'),('rock','Rock'),('country','Country')]
+    )
+    
